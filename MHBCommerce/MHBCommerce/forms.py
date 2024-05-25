53bfade77273d65af55e74ea44ffca9ccf1e78b9
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-05-25 05:55:29 +0330

    update contact form using django forms
    update contact form with validation (which is not working) and widgets

--- MHBCommerce/MHBCommerce/forms.py
+++ MHBCommerce/MHBCommerce/forms.py
@@ -2,6 +2,33 @@ from django import forms
 
 
 class ContactForm (forms.Form):
-    fullname = forms.CharField()
-    email = forms.CharField()
-    content = forms.Textarea()
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Full Name"
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Message"
+            }
+        )
+    )
+
+    def email_validation(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be Gmail")
+        return email
--- MHBCommerce/core/views.py
+++ MHBCommerce/core/views.py
@@ -18,14 +18,15 @@ def aboutus_page(request):
 
 
 def contact_page(request):
-
-    if request.method == "POST":
-        # print(request.method)
-        # print(request.POST)
-        print(request.POST.get('Email'))
+    contact_form = ContactForm(request.POST or None)
+    if contact_form.is_valid():
+        if request.method == "POST":
+            print(request.method)
+            print(request.POST)
+            print(request.POST.get('Email'))
     context = {
         'title': "Contact Us",
         'subtitle': "Keep in touch with us",
-
+        'form': contact_form
     }
     return render(request, "Contact/view.html", context)
