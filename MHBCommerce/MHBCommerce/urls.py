70708e7e498dfca78ab3f472f584d085b6558fb3
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2024-05-25 04:53:53 +0330

    add other pages

--- MHBCommerce/MHBCommerce/urls.py
+++ MHBCommerce/MHBCommerce/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home_page
+from core.views import home_page, aboutus_page, contact_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home_page'),
+    path('/about', aboutus_page, name='about_page'),
+    path('/contact', contact_page, name='contact_page'),
 ]
--- MHBCommerce/core/views.py
+++ MHBCommerce/core/views.py
@@ -3,4 +3,21 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("This is Home Page")
+    context = {
+
+    }
+    return render(request, "homePage.html", context)
+
+
+def aboutus_page(request):
+    context = {
+
+    }
+    return render(request, "about.html", context)
+
+
+def contact_page(request):
+    context = {
+
+    }
+    return render(request, "contact.html", context)
